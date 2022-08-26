19f95c21c1be853c8ffe4799d0f8e23d60c7b620
LeeHunUk <gnsdnr1022@gmail.com>
2022-08-26 20:32:23 +0900

    자료형과 변수

---
+++ Part1.파이썬-필수-문법/자료형과변수/01.자료형.py
@@ -0,0 +1,25 @@
+# - 숫자 자료형
+# 1. 정수형 : 소수점이 없는 수
+print(1)
+print(3)
+print(0)
+print(-1)
+
+# 2. 실수형 : 소수점이 있는 수
+print(1.1, 3.14, -1.4)
+
+# - 문자열 자료형
+# "" or ''
+
+print("문자열 자료형입니다.")
+print('문자열 자료형입니다.')
+
+# - 불린형 자료형
+# True or False
+print(True, False)
+
+
+# 참고
+# 중간 기호 : sep, 마지막 : end
+print(1, 1, 3, sep="|", end="")
+print("??")
---
+++ Part1.파이썬-필수-문법/자료형과변수/02.변수.py
@@ -0,0 +1,16 @@
+# 변수
+# 변수이름 = 데이터
+
+name = "master"
+level = 5
+health = 800
+attack = 90
+
+print(name, level, health, attack)
+
+# 변수 데이터 변경
+level = 6
+health = health + 100
+attack = 101
+
+print(name, level, health, attack)
